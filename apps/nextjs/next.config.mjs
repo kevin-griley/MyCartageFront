68156e536a4e9f7ee9eaad2860cb34403e4eeb3c
Kevin Griley <kevin@griley.app>
2023-04-10 16:13:12 -0700

    adding contentlayer

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,11 +1,28 @@
-/** @type {import("next").NextConfig} */
+/** @typedef {import('next').NextConfig} NextConfig */
+/** @typedef {import('webpack').Configuration} WebpackConfig */
+
+import { withContentlayer } from "next-contentlayer";
+
+/** @type {NextConfig} */
 const config = {
   reactStrictMode: true,
+
+  webpack(/** @type {WebpackConfig} */ config) {
+    config.infrastructureLogging = { level: "error" };
+    return config;
+  },
+
   /** Enables hot reloading for local packages without a build step */
   transpilePackages: [],
+
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: !!process.env.CI },
   typescript: { ignoreBuildErrors: !!process.env.CI },
 };
 
-export default config;
+/** @type {Array<(config: NextConfig) => NextConfig>} */
+const plugins = [withContentlayer];
+
+export default plugins.reduce((config, plugin) => {
+  return plugin(config);
+}, config);
