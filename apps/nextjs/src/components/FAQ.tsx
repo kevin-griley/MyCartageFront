8766c480b0587c412edc4037ca7430a7cd655bf4
= <=>
2023-04-13 16:51:04 -0700

    small

--- apps/nextjs/src/components/FAQ.tsx
+++ apps/nextjs/src/components/FAQ.tsx
@@ -32,19 +32,19 @@ const faqs = [
 export function FAQ() {
   return (
     <section id="FAQ">
-      <div className="bg-gray-50">
+      <div className="bg-gray-900">
         <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
-          <div className="mx-auto max-w-4xl divide-y divide-gray-900/10">
-            <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">
+          <div className="mx-auto max-w-4xl divide-y divide-white/10">
+            <h2 className="text-2xl font-bold leading-10 tracking-tight text-white">
               Frequently asked questions
             </h2>
-            <dl className="mt-10 space-y-6 divide-y divide-gray-900/10">
+            <dl className="mt-10 space-y-6 divide-y divide-white/10">
               {faqs.map((faq) => (
                 <Disclosure as="div" key={faq.question} className="pt-6">
                   {({ open }) => (
                     <>
                       <dt>
-                        <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
+                        <Disclosure.Button className="flex w-full items-start justify-between text-left text-white">
                           <span className="text-base font-semibold leading-7">
                             {faq.question}
                           </span>
@@ -64,7 +64,7 @@ export function FAQ() {
                         </Disclosure.Button>
                       </dt>
                       <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                        <p className="text-base leading-7 text-gray-600">
+                        <p className="text-base leading-7 text-gray-300">
                           {faq.answer}
                         </p>
                       </Disclosure.Panel>
