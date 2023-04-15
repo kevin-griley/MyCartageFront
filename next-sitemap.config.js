f169483c0dfa7c9f6fcd1ee0b282315a3460e370
Kevin Griley <kevin@griley.app>
2023-04-14 17:25:01 -0700

    updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_APP_URL,
+  siteUrl: "https://www.mycartage.com",
   generateRobotsTxt: true,
   sourceDir: "apps/nextjs/.next/",
   outDir: "apps/nextjs/public/",
