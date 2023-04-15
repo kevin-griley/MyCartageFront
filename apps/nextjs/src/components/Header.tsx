df3ffbff0d3180abbd18224da476d86bfe29a005
Kevin Griley <kevin@griley.app>
2023-04-15 09:58:45 -0700

    small fix

--- apps/nextjs/src/components/Header.tsx
+++ apps/nextjs/src/components/Header.tsx
@@ -156,7 +156,7 @@ export function Header() {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 translate-y-1"
             >
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
                 <div className="p-4">
                   {products.map((item, index) => (
                     <div
